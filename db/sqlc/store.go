80e1f6adc09dde8162ce82fd0b6d6b4c0206f67b
Johann Suarez <johann@gohash.tech>
2022-06-21 16:13:36 -0600

    Debugging deadlock error

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -92,7 +92,54 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
             Amount: +arg.Amount,
         })
 
-        // TODO: Update accounts' balance
+
+
+
+
+        account1, err := q.GetAccount(ctx, arg.FromAccountID)
+        if err != nil {
+            return err
+        }
+
+        result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+            ID: arg.FromAccountID,
+            Balance: account1.Balance - arg.Amount,
+        })
+
+        if err != nil {
+            return err
+        }
+
+
+
+
+
+
+
+
+        account2, err := q.GetAccount(ctx, arg.ToAccountID)
+        if err != nil {
+            return err
+        }
+
+        result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+            ID: arg.ToAccountID,
+            Balance: account2.Balance + arg.Amount,
+        })
+
+        if err != nil {
+            return err
+        }
+
+
+
+
+
+
+
+
+
+
 
         return nil
     })
