81b12e1ee4efe05bbe11f4ce667fb190071afaf5
Johann Suarez <johann@gohash.tech>
2022-06-29 03:23:16 -0600

    Loading environment configs properly, implenented Gin API

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "log"
+    "database/sql"
+
+    _ "github.com/lib/pq"
+    "github.com/JohannSuarez/GoBackend/api"
+    "github.com/JohannSuarez/GoBackend/util"
+    db "github.com/JohannSuarez/GoBackend/db/sqlc"
+)
+
+/*
+const (
+    dbDriver = "postgres"
+    dbSource = "postgresql://root:esth3r@localhost:5432/simple_bank?sslmode=disable"
+    serverAddress = "0.0.0.0:8080"
+)
+*/
+
+func main() {
+
+    config, err := util.LoadConfig(".")
+
+    if err != nil {
+        log.Fatal("cannot load config", err)
+    }
+
+
+    conn, err := sql.Open(config.DBDriver, config.DBSource)
+    if err != nil {
+        log.Fatal("Cannot connect to db", err)
+
+    }
+
+    store := db.NewStore(conn)
+    server := api.NewServer(store)
+
+
+    err = server.Start(config.ServerAddress)
+    if err != nil {
+        log.Fatal("Cannot start server:", err)
+
+    }
+}
---
+++ util/config.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+    "github.com/spf13/viper"
+)
+// This will store all configurations of the application.
+// Viper reads the values from a config file or environment variables.
+type Config struct {
+    DBDriver      string `mapstructure:"DB_DRIVER"`
+    DBSource      string `mapstructure:"DB_SOURCE"`
+    ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reads configurations from a config file
+// or environment variables
+func LoadConfig(path string) (config Config, err error) {
+    viper.AddConfigPath(path)
+    viper.SetConfigName("app")
+    viper.SetConfigType("env") // json, xml, toml, etc..
+
+    viper.AutomaticEnv()
+
+    err = viper.ReadInConfig()
+    if err != nil {
+        return
+    }
+
+    err = viper.Unmarshal(&config)
+    return
+}
